87725a581fb1d2cb15d3803d82df849959b754df
dev-bhaveshye <Bhaveshyatwork@gmail.com>
2025-08-26 20:42:25 +0530

    some changes in api

--- src/main/java/com/bhaveshit/journal/controller/JournalEntryController.java
+++ src/main/java/com/bhaveshit/journal/controller/JournalEntryController.java
@@ -4,11 +4,15 @@ package com.bhaveshit.journal.controller;
 import com.bhaveshit.journal.entity.JournalEntry;
 import com.bhaveshit.journal.repository.JournalEntryRepository;
 import com.bhaveshit.journal.service.JournalEntryService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/journal")
@@ -20,15 +24,54 @@ public class JournalEntryController {
     private JournalEntryService journalEntryService;
 
     @PostMapping
-    public boolean save(@RequestBody JournalEntry journalEntry) {
-        journalEntry.setDate(LocalDateTime.now());
-        journalEntryRepository.save(journalEntry);
-        return true;
+    public ResponseEntity<JournalEntry> save(@RequestBody JournalEntry journalEntry) {
+        try{
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+            return new ResponseEntity<>(journalEntry, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
     @GetMapping
-    public List<JournalEntry> getJournalEntry() {
-        return journalEntryService.getAll();
+    public  ResponseEntity<List<JournalEntry>> getJournalEntry() {
+
+        List<JournalEntry> journalEntry = journalEntryService.getAll();
+        if(!journalEntry.isEmpty()) {
+            return new ResponseEntity<>(journalEntry, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+
+    @GetMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> getJournalEntry(@PathVariable ObjectId myId) {
+        JournalEntry journalentry = journalEntryService.getById(myId);
+
+        if(journalentry!=null) {
+            return new ResponseEntity<>(journalentry, HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @DeleteMapping("id/{myId}")
+    public ResponseEntity<Object> deleteJournalEntry(@PathVariable ObjectId myId) {
+        journalEntryRepository.deleteById(myId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("id/{id}")
+    public ResponseEntity<JournalEntry> update(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry) {
+        JournalEntry updatedEntry = journalEntryService.updateById(id, newEntry);
+        return updatedEntry != null
+                ? ResponseEntity.ok(updatedEntry)
+                : ResponseEntity.notFound().build();
+    }
+
 
 
 
--- src/main/java/com/bhaveshit/journal/service/JournalEntryService.java
+++ src/main/java/com/bhaveshit/journal/service/JournalEntryService.java
@@ -2,6 +2,7 @@ package com.bhaveshit.journal.service;
 
 import com.bhaveshit.journal.entity.JournalEntry;
 import com.bhaveshit.journal.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,24 @@ public class JournalEntryService {
 
     public List<JournalEntry> getAll(){
         return journalEntryRepository.findAll();
+    }
+
+    public JournalEntry getById(ObjectId id) {
+        return journalEntryRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
+    }
+
+    public JournalEntry updateById(ObjectId id , JournalEntry newEntry) {
+
+        JournalEntry old = journalEntryRepository.findById(id).orElse(null);
+        if(old != null ){
+            old.setTitle(newEntry.getTitle()!=null && !newEntry.getTitle().equals("")?newEntry.getTitle():old.getTitle());
+            old.setContent(newEntry.getContent()!=null && !newEntry.getContent().equals("")?newEntry.getContent():old.getContent());
+
+        }
+        return journalEntryRepository.save(old);
     }
 }
